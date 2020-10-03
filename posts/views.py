aedad967dbb906514cc54642b660eaf52bbd64c6
kazunari <kz.3.nns1017@gmail.com>
2020-10-03 20:03:21 +0900

    Add admin

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 def index(request):
     #return HttpResponse("Hello, World!")
-    return render(request, 'posts/index.html')
+    posts = Post.objects.order_by('-published') #models.pyのPostクラスの内容を取得し、投稿日でソートしてpostsに格納
+    return render(request, 'posts/index.html', {'posts': posts}) #レンダリング
